79da1ef55a9881c1dc10029314e4a48945c7acab
Tomas Rabazo <tomasrabazo@gmail.com>
2023-05-21 18:20:14 +0100

    wso2mi dashboard

---
+++ wso2mi-dashboard-4.2.0/backup/server/www/conf/config.js
@@ -0,0 +1,13 @@
+window.sso = {
+    enable: false,
+    config: {
+    	clientID: "",
+    	serverOrigin: "",
+    	resourceServerURLs: [],
+    	signInRedirectURL: "",
+    	storage: "webWorker",
+    },
+    usernameAttribute: "sub",
+    adminGroupAttribute: "",
+    allowedAdminGroups: [],
+};
